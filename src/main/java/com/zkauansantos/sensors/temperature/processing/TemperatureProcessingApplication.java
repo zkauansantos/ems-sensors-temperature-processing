3c9dd8f4bf6e74ad451645bceda2635c4ba4fbbf
Kauan Santos <kauansantosdepontes@gmail.com>
2026-09-18 15:17:15 -0300

    chore: init all submodules

---
+++ src/main/java/com/zkauansantos/sensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.zkauansantos.sensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkauansantos/sensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkauansantos.sensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
